6c5b8b59249bbacb1ee5704530baaaae4a59d320
Anuj Pawadia <124757784+AnujPawaadia@users.noreply.github.com>
2024-03-12 19:12:55 +0530

    day47.java
    Let's play a game on an array! You're standing at index  of an -element array named . From some index  (where ), you can perform one of the following moves:
    
    Move Backward: If cell  exists and contains a , you can walk back to cell .
    Move Forward:
    If cell  contains a zero, you can walk to cell .
    If cell  contains a zero, you can jump to cell .
    If you're standing in cell  or the value of , you can walk or jump off the end of the array and win the game.
    In other words, you can move from index  to index , , or  as long as the destination index is a cell containing a . If the destination index is greater than , you win the game.
    
    Function Description
    
    Complete the canWin function in the editor below.
    
    canWin has the following parameters:
    
    int leap: the size of the leap
    int game[n]: the array to traverse
    Returns
    
    boolean: true if the game can be won, otherwise false

---
+++ day47.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class ArrayGame {
+
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int q = input.nextInt();
+
+    for (int i = 0; i < q; i++) {
+        int n = input.nextInt();
+        int[] list = new int[n];
+        int leap = input.nextInt();
+        for (int p = 0; p < n; p++) {
+            int t = input.nextInt();
+            list[p] = t;
+        }
+        if (canWin(leap, list)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
+
+public static boolean canWin(int leap, int[] game) {
+    return canWinHelper(0, leap, game);
+}
+
+private static boolean canWinHelper(int position, int leap, int[] game) {
+    if (position < 0 || game[position] == 1) {
+        return false;
+    }
+    if (position == game.length - 1 || position + leap >= game.length) {
+        return true;
+    }
+    game[position] = 1; // Mark as visited
+    return canWinHelper(position + leap, leap, game) || canWinHelper(position + 1, leap, game) || canWinHelper(position - 1, leap, game);
+}
+}
